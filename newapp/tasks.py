09ad10dff1560cd6d68fcbc0e15b5746cab4bc3b
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-08-08 10:28:45 +0600

    async task

--- newapp/tasks.py
+++ newapp/tasks.py
@@ -3,6 +3,8 @@ from celery import shared_task
 import time
 
 
+# synchronous task execution
+
 @shared_task
 def add(x, y, queue='celery', task_rate_limit='1/m'):
     time.sleep(3)
@@ -25,3 +27,17 @@ def div(x, y, queue='celery:2'):
 def mod(x, y, queue='celery:3'):
     time.sleep(3)
     return x % y
+
+
+
+# asynchronous tasks execution
+def execute_sync():
+    result = add.apply_async(args=[5, 10], kwargs={"message": "The sum is"})
+    task_result = result.get()
+    print("Task is running synchronously")
+    print(task_result)
+
+def execute_async():
+    result = mul.apply_async(args=[5, 10], kwargs={"message": "The sum is"})
+    print("Task is running asynchronously")
+    print("Task ID:", result.task_id)
